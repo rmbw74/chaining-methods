1299b9e2d3dbb1c1e64b1c13399a356aff149e14
Ray Medrano <rmbw74@gmail.com>
2017-10-23 19:05:14 -0500

    Oh my god my brain is melting

---
+++ chaining.js
@@ -0,0 +1,3 @@
+const integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
+
+let FinalArray = console.log(integers.sort(function(prev, next){return next - prev}).filter(function(X){return X > 19}).map(function(X){return ((X*1.5)-1)}).reduce(function(total,sum){return(total + sum)}))
